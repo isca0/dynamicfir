3f49dd808cadb38bb6274d7d3dfe13a9bfb54238
isca <igorsca@gmail.com>
2015-01-16 17:31:45 -0200

    The first version of dynamicfir.sh

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # dynamicfir
-A dynamic host DNAT iptables front-end
+=================
+
+### A dynamic host DNAT iptables front-end for Network Unix Administrator's
+
+
+By: [Zero](http://plebeos1.blogspot.com)
+Curitiba/PR - Brazil (13/09/2014)
+
+
+## the need
+
+####english explain comes first ;)
+A lot o people request a way to access network services from remote computers without vpn. On a statefull firewall we allways dont want unidentified hosts to connect on network. And on small networks we dont wnat a lot of resources to make a remote client get in on some service port. So i just make a way to simplified the regular dynamichost users to access this services allowing only the dynamic ip address to be constantly verified on firewall and create the chain on iptables.
+
+## Required
+
+* Cron (must be runnig as an cron job)
+* host (host command line tool)
+
+## Knolegment
+
+Its only a simple shell script, i presume you have a little knolegment with Linux network administration, and iptables basic's
+
+Ps.: Right Now, this only support's a simple DNAT redirection
---
+++ dynamicfir.sh
@@ -0,0 +1,131 @@
+#!/bin/sh
+#By:Zz0
+#zz0@garagemhacker.org
+#19/08/2014
+#This script has the objective to update iptables rules for ddns users.
+#If you need to insert dynamic hosts on your firewall rules this is made for U
+#Just change the global vars for your necessit, and enjoy ;)
+
+#---------------
+###GLOBAL VARS###
+#---------------
+
+#Begin of the clients
+
+client[0]="somehost.sytes.net"
+client[1]="anotherhost.ddns.net"
+client[2]="onemoresample.ddns.net"
+
+#Change "xxx" to the ip of Destination NAT
+#Sample: acessos[0]="192.168.1.253"
+acessos[0]="xxx.xxx.xxx.xxx"
+
+#End of the clients
+
+#iptables vars
+#Port of incoming connection
+#Like ts sample must be like: inport[0]="3389"
+inport[0]="0000"
+
+IP=/usr/sbin/iptables
+SAVE=/usr/sbin/iptables-save
+
+#Change this for your WAN port
+PPP=ppp0
+#----------------------
+###DONT CHANGE AHEAD###
+#----------------------
+
+dynamicfile="/tmp/dynamicfir0"
+newipfile="/tmp/newipfile0"
+oldipfile="/tmp/oldipfile0"
+oldipfileb="/tmp/oldipfile1"
+array=0
+
+myip(){
+	while [ $array != ${#client[@]} ]
+	do
+		host -4 ${client[$array]}|cut -d' ' -f4
+		let "array=$array + 1"
+	done
+
+} 
+
+makefiles(){
+	if [ ! -e "$dynamicfile" ]
+	then
+		myip > "$dynamicfile"
+	else
+		myip > "$newipfile"
+	fi
+}
+
+oldip(){
+	if [ ! -e "$oldipfile" ]
+	then
+		makefiles
+	fi
+	diff -u "$dynamicfile" "$newipfile" > "$oldipfile"
+	grep '^-[0-9]' "$oldipfile"|cut -c2- > "$oldipfileb"
+	mv -f "$oldipfileb" "$oldipfile"
+	
+}	
+
+insert_ts_nat(){
+	while read ips
+	do
+		$IP -t nat -A PREROUTING -i $PPP -s $ips -p tcp --dport ${inport[0]} -j DNAT --to ${acessos[0]} 
+	done < "$dynamicfile"
+}
+
+remove_ts_nat(){
+	while read oldips
+	do
+		$IP -t nat -D PREROUTING -i $PPP -s $oldips -p tcp --dport ${inport[0]} -j DNAT --to ${acessos[0]} 
+	done < "$oldipfile"
+}
+
+fir_compare(){
+	while read validos
+	do
+		"$SAVE"|grep "$validos"
+		if [ $? -eq 0 ]
+		then
+			echo "$validos ja esta cadastrado"
+			continue
+		else
+			insert_ts_nat
+		fi
+	done < "$dynamicfile"
+
+}	
+
+
+#-----------------
+### rc-section ####
+#-----------------
+if [ ! -e "$dynamicfile" ]
+then
+	makefiles
+else
+	oldip
+fi
+
+if [ -s "$oldipfile" ]
+then
+	remove_ts_nat
+	mv "$newipfile" "$dynamicfile"
+	rm -rf "$oldipfile"
+	fir_compare
+	echo "Os arquivos mudaram, atualizei regras"
+else
+	rm -rf "$oldipfile"
+	rm -rf "$newipfile"
+	fir_compare
+	echo "Os IP's nao mudaram, nao farei nada"
+fi
+
+
+
+
+
